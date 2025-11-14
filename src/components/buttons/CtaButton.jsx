8e2116719583ec8884eb6dfb3714dfeba02d2fdb
Edison Matos <edison.matos@live.com>
2025-11-14 15:23:08 -0300

    feat: Insere componente de CtaButton em todos os lugares

--- src/components/buttons/CtaButton.jsx
+++ src/components/buttons/CtaButton.jsx
@@ -1,3 +1,25 @@
-export default function CtaButton() {
-  return <></>;
+import MotionDivDownToUp from "../animations/MotionDivDownToUp";
+
+export default function CtaButton({ link, icon, label, colorMode }) {
+  const themes = {
+    light: "bg-lighter text-fontDarker ",
+    dark: "bg-darker text-fontLighter",
+  };
+
+  const colors = themes[colorMode] || themes.light;
+
+  return (
+    <>
+      <MotionDivDownToUp>
+        <a
+          target="_blank"
+          href={link}
+          className={`inline-flex items-center justify-center ${colors} font-semibold px-6 py-3 rounded-lg text-lg transition-all hover:scale-105 shadow-lg gap-3`}
+        >
+          {icon}
+          {label}
+        </a>
+      </MotionDivDownToUp>
+    </>
+  );
 }
