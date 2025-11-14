bf79354b2c5c0baaab4937b77e6b07b5c377de9d
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-14 11:39:29 -0300

    fix: Animação no Cta

--- src/sections/Cta.jsx
+++ src/sections/Cta.jsx
@@ -1,3 +1,4 @@
+import MotionDivDownToUp from "../components/Animations/MotionDivDownToUp";
 import content from "../content/Content";
 
 export default function Cta() {
@@ -13,50 +14,58 @@ export default function Cta() {
           <div class="absolute inset-0 bg-black/90"></div>
         </div>
         <div className="relative z-10 px-4 py-16 mx-auto text-center max-w-7xl sm:py-24 sm:px-6 lg:px-8">
-          <span className="inline-block px-4 py-1 text-xs font-semibold tracking-wider text-white uppercase rounded-full bg-darker">
-            {content.cta.texts.etiqueta}
-          </span>
+          <MotionDivDownToUp>
+            <span className="inline-block px-4 py-1 text-xs font-semibold tracking-wider text-white uppercase rounded-full bg-darker">
+              {content.cta.texts.etiqueta}
+            </span>
+          </MotionDivDownToUp>
 
-          <h2 className="max-w-3xl mx-auto mt-6 text-3xl font-bold text-white lg:text-4xl">
-            {content.cta.texts.titulo}
-          </h2>
+          <MotionDivDownToUp>
+            <h2 className="max-w-3xl mx-auto mt-6 text-3xl font-bold text-white lg:text-4xl">
+              {content.cta.texts.titulo}
+            </h2>
+          </MotionDivDownToUp>
 
           <div className="max-w-3xl p-8 mx-auto mt-10 shadow-lg rounded-2xl sm:p-12">
-            <div className="inline-block text-left">
-              <ul className="space-y-5">
-                {content.cta.pontos
-                  .filter((ponto) => ponto.index !== 0)
-                  .map((ponto) => (
-                    <li key={ponto.index} className="flex items-center">
-                      <svg
-                        className="flex-shrink-0 w-6 h-6 text-lighter"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+            <MotionDivDownToUp>
+              <div className="inline-block text-left">
+                <ul className="space-y-5">
+                  {content.cta.pontos
+                    .filter((ponto) => ponto.index !== 0)
+                    .map((ponto) => (
+                      <li key={ponto.index} className="flex items-center">
+                        <svg
+                          className="flex-shrink-0 w-6 h-6 text-lighter"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
 
-                      <span className="ml-3 text-lg text-white">
-                        {ponto.texto}
-                      </span>
-                    </li>
-                  ))}
-              </ul>
-            </div>
+                        <span className="ml-3 text-lg text-white">
+                          {ponto.texto}
+                        </span>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </MotionDivDownToUp>
 
-            <div className="mt-10">
-              <a
-                href="https://wa.me/+5511990260977?text=Ol%C3%A1!%20Vim%20do%20Google%20e%20gostaria%20de%20saber%20mais.%20Pode%20me%20ajudar? ⚖️⚖️"
-                className="inline-block px-8 py-3 text-lg font-semibold text-black transition-all rounded-lg shadow-md hover:scale-105 bg-lighter "
-              >
-                {content.cta.texts.labelBotaoPrincial}
-              </a>
-            </div>
+            <MotionDivDownToUp>
+              <div className="mt-10">
+                <a
+                  href="https://wa.me/+5511990260977?text=Ol%C3%A1!%20Vim%20do%20Google%20e%20gostaria%20de%20saber%20mais.%20Pode%20me%20ajudar? ⚖️⚖️"
+                  className="inline-block px-8 py-3 text-lg font-semibold text-black transition-all rounded-lg shadow-md hover:scale-105 bg-lighter "
+                >
+                  {content.cta.texts.labelBotaoPrincial}
+                </a>
+              </div>
+            </MotionDivDownToUp>
           </div>
         </div>
       </div>
--- src/sections/Transformacao.jsx
+++ src/sections/Transformacao.jsx
@@ -1,3 +1,4 @@
+import MotionDivDownToUp from "../components/Animations/MotionDivDownToUp";
 import CardTransformacao from "../components/CardTransformacao";
 import content from "../content/Content";
 
@@ -6,59 +7,65 @@ export default function Transformacao() {
     <>
       <div className="bg-white">
         <div className="px-4 py-16 mx-auto max-w-7xl sm:py-24 sm:px-6 lg:px-8">
-          <div className="max-w-3xl mx-auto mb-16 text-center">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 lg:text-4xl">
-              {content.transformacao.texts.titulo}
-            </h2>
-          </div>
+          <MotionDivDownToUp>
+            <div className="max-w-3xl mx-auto mb-16 text-center">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 lg:text-4xl">
+                {content.transformacao.texts.titulo}
+              </h2>
+            </div>
+          </MotionDivDownToUp>
 
-          {(() => {
-            const cardsConfig = [
-              {
-                key: "antes",
-                data: content.transformacao.antes,
-                styles: {
-                  cardClasses:
-                    "bg-neutral-50/90 shadow-lg hover:shadow-xl transition-all duration-300",
-                  etiquetaClasses: "text-red-700 bg-red-100",
-                  bulletClasses: "bg-red-500",
-                  textoClasses: "text-lg text-gray-700",
+          <MotionDivDownToUp>
+            {(() => {
+              const cardsConfig = [
+                {
+                  key: "antes",
+                  data: content.transformacao.antes,
+                  styles: {
+                    cardClasses:
+                      "bg-neutral-50/90 shadow-lg hover:shadow-xl transition-all duration-300",
+                    etiquetaClasses: "text-red-700 bg-red-100",
+                    bulletClasses: "bg-red-500",
+                    textoClasses: "text-lg text-gray-700",
+                  },
                 },
-              },
-              {
-                key: "depois",
-                data: content.transformacao.depois,
-                styles: {
-                  cardClasses:
-                    "bg-neutral-50/90 shadow-lg hover:shadow-xl transition-all duration-300",
-                  etiquetaClasses: "text-green-700 bg-green-100",
-                  bulletClasses: "bg-green-800",
-                  textoClasses: "text-lg font-medium text-gray-900",
+                {
+                  key: "depois",
+                  data: content.transformacao.depois,
+                  styles: {
+                    cardClasses:
+                      "bg-neutral-50/90 shadow-lg hover:shadow-xl transition-all duration-300",
+                    etiquetaClasses: "text-green-700 bg-green-100",
+                    bulletClasses: "bg-green-800",
+                    textoClasses: "text-lg font-medium text-gray-900",
+                  },
                 },
-              },
-            ];
+              ];
 
-            return (
-              <div className="grid max-w-5xl grid-cols-1 gap-8 mx-auto md:grid-cols-2">
-                {cardsConfig.map((config) => (
-                  <CardTransformacao
-                    key={config.key}
-                    data={config.data}
-                    styles={config.styles}
-                  />
-                ))}
-              </div>
-            );
-          })()}
+              return (
+                <div className="grid max-w-5xl grid-cols-1 gap-8 mx-auto md:grid-cols-2">
+                  {cardsConfig.map((config) => (
+                    <CardTransformacao
+                      key={config.key}
+                      data={config.data}
+                      styles={config.styles}
+                    />
+                  ))}
+                </div>
+              );
+            })()}
+          </MotionDivDownToUp>
 
-          <div className="mt-16 text-center">
-            <a
-              href="https://wa.me/+5511990260977?text=Ol%C3%A1!%20Vim%20do%20Google%20e%20gostaria%20de%20saber%20mais.%20Pode%20me%20ajudar? ⚖️⚖️"
-              className="inline-block px-8 py-3 text-lg font-semibold text-white transition-all rounded-lg shadow-md hover:scale-105 bg-darker"
-            >
-              {content.transformacao.texts.labelBotaoPrincial}
-            </a>
-          </div>
+          <MotionDivDownToUp>
+            <div className="mt-16 text-center">
+              <a
+                href="https://wa.me/+5511990260977?text=Ol%C3%A1!%20Vim%20do%20Google%20e%20gostaria%20de%20saber%20mais.%20Pode%20me%20ajudar? ⚖️⚖️"
+                className="inline-block px-8 py-3 text-lg font-semibold text-white transition-all rounded-lg shadow-md hover:scale-105 bg-darker"
+              >
+                {content.transformacao.texts.labelBotaoPrincial}
+              </a>
+            </div>
+          </MotionDivDownToUp>
         </div>
       </div>
     </>
