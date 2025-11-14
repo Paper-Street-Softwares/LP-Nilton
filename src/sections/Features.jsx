ce6cea4b66fd425321b791fd0d4d80f584519ffa
Edison Matos <edison.matos@live.com>
2025-11-14 16:58:55 -0300

    feat: Coloca de volta o motion div

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,3 +1,4 @@
+import MotionDivDownToUp from "../components/animations/MotionDivDownToUp";
 import CtaButton from "../components/buttons/CtaButton";
 import CardFeatures from "../components/cards/CardFeatures";
 import SectionArea from "../components/sectionElements/SectionArea";
@@ -9,9 +10,11 @@ export default function Features() {
       <div className="bg-gray-100">
         <SectionArea>
           <div className="flex flex-col items-center px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <h2 className="mb-16 text-3xl font-bold tracking-tight text-center text-fontDarker lg:text-4xl">
-              {content.features.texts.titulo}
-            </h2>
+            <MotionDivDownToUp>
+              <h2 className="mb-16 text-3xl font-bold tracking-tight text-center text-fontDarker lg:text-4xl">
+                {content.features.texts.titulo}
+              </h2>
+            </MotionDivDownToUp>
 
             <div className="flex flex-col gap-10 mb-16 md:flex-row md:flex-wrap md:justify-center">
               {content.features.cards
