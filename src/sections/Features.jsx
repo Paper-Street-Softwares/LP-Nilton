2840e6c752ad19a0f43fd1966d3cfd9163329184
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-14 11:34:34 -0300

    fix: Animação nos itens da Features

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,3 +1,4 @@
+import MotionDivDownToUp from "../components/Animations/MotionDivDownToUp";
 import CardFeatures from "../components/CardFeatures";
 import content from "../content/Content";
 
@@ -6,26 +7,32 @@ export default function Features() {
     <>
       <div className="bg-gray-100">
         <div className="px-4 py-16 mx-auto max-w-7xl sm:py-24 sm:px-6 lg:px-8">
-          <h2 className="mb-16 text-3xl font-bold tracking-tight text-center text-gray-900 lg:text-4xl">
-            {content.features.texts.titulo}
-          </h2>
+          <MotionDivDownToUp>
+            <h2 className="mb-16 text-3xl font-bold tracking-tight text-center text-gray-900 lg:text-4xl">
+              {content.features.texts.titulo}
+            </h2>
+          </MotionDivDownToUp>
 
-          <div className="flex flex-col gap-10 md:flex-row md:flex-wrap md:justify-center">
-            {content.features.cards
-              .filter((card) => card.index !== 0)
-              .map((card) => (
-                <CardFeatures key={card.index} {...card} />
-              ))}
-          </div>
+          <MotionDivDownToUp>
+            <div className="flex flex-col gap-10 md:flex-row md:flex-wrap md:justify-center">
+              {content.features.cards
+                .filter((card) => card.index !== 0)
+                .map((card) => (
+                  <CardFeatures key={card.index} {...card} />
+                ))}
+            </div>
+          </MotionDivDownToUp>
 
-          <div className="mt-16 text-center">
-            <a
-              href="https://wa.me/+5511990260977?text=Ol%C3%A1!%20Vim%20do%20Google%20e%20gostaria%20de%20saber%20mais.%20Pode%20me%20ajudar? ⚖️⚖️"
-              className="inline-block px-8 py-3 text-lg font-semibold text-white transition-all rounded-lg shadow-md hover:scale-105 bg-darker"
-            >
-              {content.features.texts.labelBotaoPrincial}
-            </a>
-          </div>
+          <MotionDivDownToUp>
+            <div className="mt-16 text-center">
+              <a
+                href="https://wa.me/+5511990260977?text=Ol%C3%A1!%20Vim%20do%20Google%20e%20gostaria%20de%20saber%20mais.%20Pode%20me%20ajudar? ⚖️⚖️"
+                className="inline-block px-8 py-3 text-lg font-semibold text-white transition-all rounded-lg shadow-md hover:scale-105 bg-darker"
+              >
+                {content.features.texts.labelBotaoPrincial}
+              </a>
+            </div>
+          </MotionDivDownToUp>
         </div>
       </div>
     </>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -72,23 +72,22 @@ export default function Hero() {
             </MotionDivDownToUp>
           </div>
           <div class="absolute bottom-6 left-1/2 -translate-x-1/2">
-          <MotionDivDownToUp>
+            <MotionDivDownToUp>
               <svg
-              class="h-8 w-8 text-white animate-bounce"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-          </MotionDivDownToUp>
-          
+                class="h-8 w-8 text-white animate-bounce"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </MotionDivDownToUp>
           </div>
         </div>
       </div>
