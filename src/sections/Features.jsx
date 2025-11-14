22eee44d8f9cb542dc486d9780fe6bb780fe3cfd
Edison Matos <edison.matos@live.com>
2025-11-14 11:08:02 -0300

    feat: Muda cor do texto

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -21,7 +21,7 @@ export default function Features() {
           <div className="mt-16 text-center">
             <a
               href="https://wa.me/+5511990260977?text=Ol%C3%A1!%20Vim%20do%20Google%20e%20gostaria%20de%20saber%20mais.%20Pode%20me%20ajudar? ⚖️⚖️"
-              className="inline-block px-8 py-3 text-lg font-semibold text-black transition-all rounded-lg shadow-md hover:scale-105 bg-lighter"
+              className="inline-block px-8 py-3 text-lg font-semibold text-white transition-all rounded-lg shadow-md hover:scale-105 bg-darker"
             >
               {content.features.texts.labelBotaoPrincial}
             </a>
