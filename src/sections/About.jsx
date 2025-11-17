45c7846cddb4cfda8083ada829ee6ffcc7bb717a
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-17 11:25:45 -0300

    fix: Ajustes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,22 +7,22 @@ import content from "../content/Content";
 export default function About() {
   return (
     <>
-      <div className="bg-neutral-100">
+      <div className="bg-neutral-100 ">
         <SectionArea>
-          <div className="flex flex-col items-center mx-auto max-w-7xl">
+          <div className="flex flex-col items-center max-w-7xl">
             <SectionHeader
               title={content.about.texts.titulo}
               marginBottom="mb-0"
             />
 
             <MotionDivDownToUp>
-              <div className="flex flex-col max-w-6xl gap-8 p-8 mx-auto bg-white border border-gray-100 shadow-2xl md:gap-16 rounded-2xl sm:p-12 lg:flex-row">
-                <MotionDivDownToUp>
+              <div className="flex flex-col max-w-6xl gap-8 p-8 bg-white border border-gray-100 shadow-2xl md:gap-16 rounded-2xl sm:p-12 lg:flex-row">
+                <MotionDivDownToUp className="">
                   <div className="flex justify-center md:items-center">
                     <img
                       src={content.about.imagem.imgPessoa}
                       alt="Imagem ilustrativa"
-                      className="w-[190px] phone1:w-[245px] phone3:w-[295px] tablet1:w-[400px] max-w-[480px] rounded-lg h-auto"
+                      className="w-[90%] phone3:w-[295px] tablet1:w-[400px] max-w-[480px] rounded-lg h-auto"
                     />
                   </div>
                 </MotionDivDownToUp>
