224cd285d4c9a255e0c34671de22a16d57ed4621
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-11-18 13:37:30 -0300

    fix: remove codigo

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -4,16 +4,8 @@ import CardFeatures from "../components/cards/CardFeatures";
 import SectionArea from "../components/sectionElements/SectionArea";
 import SectionHeader from "../components/sectionElements/SectionHeader.jsx";
 import content from "../content/Content";
-import React, { useState } from "react";
-import { Dialog } from "primereact/dialog";
-import { X, MoveRight } from "lucide-react";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 export default function Features() {
-  const [visible, setVisible] = useState(false);
-  const abrirModal = () => {
-    setVisible(true);
-  };
   return (
     <>
       <div className="bg-neutral-100">
